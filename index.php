a4e0edd4e92f35eaaf295a8dbf7b4990cbfa7aae
Ced Al <142415084+AICedric1001@users.noreply.github.com>
2023-09-20 23:05:43 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="style.css">
+    <title>Arrays!</title>
+    
+</head>
+<body>
+
+    <div class="container">
+        <h1 class="heading">An Array Activity</h1>
+        <p class="sub-heading">By Al Cedric Abarientos</p>
+        <div class="button-container">
+            <a class="button" href="arrayPush.php">Go!</a>
+        </div>
+    </div>
+</body>
+</html>
